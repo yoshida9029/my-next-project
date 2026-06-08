9296c0c73019b41bdc8903431700b2a9cbd13187
yoshida9029 <d90290711@gmail.com>
2026-06-08 16:27:32 +0900

    ニュースをSSRに適応

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -13,6 +13,8 @@ type Props = {
     }>;
 };
 
+export const revalidate = 0;
+
 export default async function Page({ params , searchParams }: Props) {
     const { slug } = await params;
     const { dk } = await searchParams;
--- app/news/page.tsx
+++ app/news/page.tsx
@@ -4,6 +4,8 @@ import { NEWS_LIST_LIMIT } from "../_constants";
 import Pagination from "../_components/Pagination/Pagination";
 import SearchField from "../_components/SearchField/SearchField";
 
+export const revalidate = 0;
+
 export default async function Page(){
     const {contents: news , totalCount } = await getNewsList({
         limit: NEWS_LIST_LIMIT,
